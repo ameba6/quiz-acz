b36c6e3f6a66a6406b4e4e7bf523199273d19e02
ameba6 <cortesa@gmail.com>
2015-06-14 21:33:00 +0200

    Sequelize.js y SQLite

---
+++ models/quiz.js
@@ -0,0 +1,6 @@
+module.exports = function(sequelize, DataTypes) {
+	return sequelize.define('Quiz',
+													{ pregunta:  {type: DataTypes.STRING},
+													  respuesta: {type: DataTypes.STRING},
+													});
+}
