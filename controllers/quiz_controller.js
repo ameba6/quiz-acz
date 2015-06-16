2e11fdb24f2262d97ce5c159edeecad6b8a9c21e
ameba6 <cortesa@gmail.com>
2015-06-16 23:11:07 +0200

    Validacion de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -22,18 +22,19 @@ exports.index = function (req, res) {
 	models.Quiz.findAll(whereObj).then(function(quizes) {
 		if (quizes.length === 0) {noQuizes= 'No hay preguntas que contengan "' + search + '"' };
 		res.render('quizes/index.ejs',{quizes: quizes,
-																	 noQuizes: noQuizes});
+																	 noQuizes: noQuizes,
+																	 errors: []});
 	}).catch(function(error) { next(error);})
 };
 
 //GET /author
 exports.author = function(req, res){
-	res.render('author', {});
+	res.render('author', {errors: []});
 };
 
 //GET /quizes/:id
 exports.show = function(req, res){
-	res.render('quizes/show', { quiz: req.quiz});
+	res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 //GET /quizes/:id/answer
@@ -43,25 +44,35 @@ exports.answer = function(req, res){
 		resultado = 'Correcto'
 	}
 	res.render('quizes/answer', {quiz: req.quiz,
-															 respuesta: resultado});
+															 respuesta: resultado, errors: []});
 };
 
 //GET /quizes/new
 exports.new = function (req, res){
 	var quiz = models.Quiz.build( // crea objeto quiz
-		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		{pregunta: "", respuesta: ""}
 	);
 	
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 //POST /quizes/create
 exports.create = function (req, res){
 	var quiz = models.Quiz.build( req.body.quiz );
 	
-	// guarda en DB los campos pregunta y respuesta del quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then( function(){
-		res.redirect('/quizes');
-	});  //Redireccion HTTP (URL relativo) lista de preguntas
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if(err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+				quiz	// save: guarda en DB los campos pregunta y respuesta del quiz
+					.save({fields: ["pregunta", "respuesta"]})
+					.then( function(){res.redirect('/quizes');});
+			}				// res.redirect: Redireccion HTTP a lista de preguntas
+		}
+	);
 };
+
 
