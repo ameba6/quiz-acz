9afa8797e2ece23c587a31a8c27873349bba535d
ameba6 <cortesa@gmail.com>
2015-06-26 21:34:16 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definicion de rutas de sesion
 router.get('/login', 	sessionController.new); 		// formulario login
@@ -33,6 +34,7 @@ router.delete('/quizes/:quizId(\\d+)',			sessionController.loginRequired, quizCo
 // Definicion de rutas de comentrios
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',			commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 // Definicion de ruta para Author
 router.get('/author', quizController.author);
